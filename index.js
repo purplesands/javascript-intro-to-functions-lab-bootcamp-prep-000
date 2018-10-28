84ce6a8eac03167ed5c014f9ae108b26105f4a62
Joe Mullen <mullenjoe13@gmail.com>
2018-10-28 16:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string)  {
   
-  if (string = lowercase.toLowerCase() === lowercase) {
+  if (string = lowercase) {
     return "I can't hear you!";
-  } else if (string = uppercase.toUpperCase() === uppercase) {
+  } else if (string = uppercase.) {
     return "YES INDEED!";
   } else if (string = mixedCase) {
     return "I love you, too";
